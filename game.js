35296c53be50a8d95d6d9af6ef5c1daa465f28ad
brr <skibidibada@users.noreply.github.com>
2024-11-17 08:34:21 +0100

    Update game.js

--- game.js
+++ game.js
@@ -12,7 +12,9 @@ let acceleration = 0.2;
 
 function preload() {
   // earth image taken from https://pngimg.com/image/25361
-  earthImage = loadImage("earthPNG.png"); //size 2579x2563
+  earthImage = loadImage(
+    "https://cdn.mos.cms.futurecdn.net/FaWKMJQnr2PFcYCmEyfiTm-1200-80.jpg"
+  ); //size 2579x2563
 
   // moon image taken from https://www.clipartmax.com/middle/m2i8H7G6i8d3Z5H7_file-moon-transparent-drawing/
   moonImage = loadImage("moonPNG.png"); // size 1000x1000
